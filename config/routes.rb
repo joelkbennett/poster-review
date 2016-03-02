695c9cbb195fbf09d8cbd30513e807308fee37f9
joelkbennett <joelkbennett@gmail.com>
2016-03-02 21:58:31 +0000

    Change index to posters#index

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
+  root to: 'posters#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
