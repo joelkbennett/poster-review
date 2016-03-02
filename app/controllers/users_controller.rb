a09a8dad61a4f61f8a738036a15aca73f70d33f4
joelkbennett <joelkbennett@gmail.com>
2016-03-02 20:11:13 +0000

    Add login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:use_id] = @user.id
       redirect_to posters_path
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   resources :posters
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
