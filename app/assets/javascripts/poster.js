885963597a1339e65b785cdcbe48ec63d0e630df
joelkbennett <joelkbennett@gmail.com>
2016-03-03 17:03:16 +0000

    Move full poster to a right side slide out

--- app/assets/javascripts/poster.js
+++ app/assets/javascripts/poster.js
@@ -1,22 +1,24 @@
 (function() {
-
   var posters = $('.poster-teasers');
   var display = $('.poster-display');
+  var inner = display.find('.poster-display-inner');
+  var body = $('body');
+
   posters.find('.image-link').on('click', function(e) {
     var id = $(this).data('id');
     var url = '/posters/' + id;
     e.preventDefault();
-    display.addClass('poster-active');
+    body.addClass('poster-active');
 
     $.ajax(url, function() {
       id: this.data('id');
     }).done(function(res) {
       $('.poster-loading').fadeOut();
       var poster = $('<img>').attr({src: res.image.url})
-      display.empty().append(poster);
-      poster.scroll();
-      poster.on('click', function() {
-        display.empty().removeClass('poster-active');
+      inner.empty().append(poster);
+      display.on('click', function() {
+        inner.empty();
+        body.removeClass('poster-active');
       });
     });
   });
