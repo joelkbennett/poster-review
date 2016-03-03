d3b5ccbbe19b98ed23b6285100ffd39181b8ee7c
joelkbennett <joelkbennett@gmail.com>
2016-03-03 04:57:31 +0000

    Add admin as a subtype of user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
 
   def index
     # @users = User.all
-    @users = User.order(:username).page(params[:page])
+    @users = User.order(:id).page(params[:page])
   end
 
   def destroy
